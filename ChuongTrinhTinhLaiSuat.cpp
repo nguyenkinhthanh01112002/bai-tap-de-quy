bf0a6b2f07079cd66e59fdd32a6427e0303fd7c9
nguyenkinhthanh01112002 <nguyenkinhthanh11@gmail.com>
2022-12-20 19:59:20 +0700

    xoa code

--- ChuongTrinhTinhLaiSuat.cpp
+++ ChuongTrinhTinhLaiSuat.cpp
@@ -12,17 +12,6 @@ void LaiSuatNganHang(){
 		n++;
 		TienGocCanTra = 1*temp/12;
 		printf("\nso tien goc can phai tra trong ky %d la %.1f",n,TienGocCanTra);
-		LaiSuatCanTra = 5*TienNo/100;
-		printf("\nso tien lai can tra trong ky %d la %.1f",n,LaiSuatCanTra);
-		TongSoTienTra = TienGocCanTra+LaiSuatCanTra;
-		printf("\nso tien thuc te can phai trong ky %d la %.1f",n,TongSoTienTra);
-		TienNo = TienNo-TienGocCanTra;
-		if(n!=12){
-			printf("\nso tien con lai sau ky %d la %.1f",n,TienNo);
-		}
-		else{
-			printf("\nso tien con lai sau ky %d la 0",n);
-		}
 	}
 }
 int main()

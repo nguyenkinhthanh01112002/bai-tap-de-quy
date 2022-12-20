d6afa31ceb923721bbcf1df43926e7244b2bfba9
nguyenkinhthanh01112002 <nguyenkinhthanh11@gmail.com>
2022-12-20 19:56:22 +0700

    anh nhan sua code

--- ChuongTrinhTinhLaiSuat.cpp
+++ ChuongTrinhTinhLaiSuat.cpp
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 //CHUONG TRINH TINH KY HANG TRA LAI
-void LaiSuatNganHang()
-{
+void LaiSuatNganHang(){
+	// chu thich nay dat cho vui
 	float TienNo,TienGocCanTra,LaiSuatCanTra,TongSoTienTra;
 	int n = 0;//Ki Han Tra Ngan Hang
 	printf("nhap vao so tien ban vay ngan hang:");
 	scanf("%f",&TienNo);
 	float temp = TienNo;
-	while(TienNo>0)
-	{
+	while(TienNo>0){
 		n++;
 		TienGocCanTra = 1*temp/12;
 		printf("\nso tien goc can phai tra trong ky %d la %.1f",n,TienGocCanTra);
@@ -18,12 +17,10 @@ void LaiSuatNganHang()
 		TongSoTienTra = TienGocCanTra+LaiSuatCanTra;
 		printf("\nso tien thuc te can phai trong ky %d la %.1f",n,TongSoTienTra);
 		TienNo = TienNo-TienGocCanTra;
-		if(n!=12)
-		{
+		if(n!=12){
 			printf("\nso tien con lai sau ky %d la %.1f",n,TienNo);
 		}
-		else
-		{
+		else{
 			printf("\nso tien con lai sau ky %d la 0",n);
 		}
 	}
